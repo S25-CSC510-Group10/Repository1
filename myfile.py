bdc8b77b42d7b9fde484b092fb05bd920126ec6b
HankLenham <81448661+HankLenham@users.noreply.github.com>
2025-01-13 17:40:05 +0000

    Committed license, readme, sample python file, and gitignores.

---
+++ myfile.py
@@ -0,0 +1,10 @@
+# Set x equal to five
+x = 5
+# Iterate five times
+while x < 10:
+    print("X is " + st(x) )
+    x = x + 1
+y = 10
+# If y is equal to x
+if y == x:
+    print("Y is equal to x!")
