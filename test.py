58f41a058f901bcfdab55d2e7bb6fccff11231e1
lastephenson <122657339+lastephenson42@users.noreply.github.com>
2025-01-14 01:24:14 +0000

    Pushing hw0 python files

---
+++ test.py
@@ -0,0 +1,9 @@
+print("Hello World!")
+for i in range(1, 10):
+    print("This is loop repetition ", i)
+
+print("We will now try and print an invalid value for x")
+print("No value has been defined for it")
+print("Here we go:")
+
+print(x)
