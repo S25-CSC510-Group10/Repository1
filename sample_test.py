616bef15ad0e1e2d5596a8d1816110387e9beac4
lastephenson <122657339+lastephenson42@users.noreply.github.com>
2025-01-14 01:42:54 +0000

    Pushing sample python test file

--- sample_test.py
+++ sample_test.py
@@ -1,7 +1,7 @@
-# content of test_sample.py
-def func(x):
-    return x + 1
-
+import myfile
 
 def test_answer():
-    assert func(3) == 5
+    assert myfile.func(3) == 5
+
+def correct_answer():
+    assert myfile.func(3) == 4
